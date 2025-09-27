23aa26bb3a9bf72fef3e43991fd9333cdc10b39b
Marielaquintanar <marielaqntrr@gmail.com>
2025-09-26 18:12:41 -0600

    changed dimensions

--- src/components/ExperienceTabs.tsx
+++ src/components/ExperienceTabs.tsx
@@ -82,16 +82,16 @@ function ExperienceTabs() {
         {activeTab === "second" && (
             <div>
                 <div style={{ display: "flex", gap: "8px", marginBottom: "4px" }}>
-                <h1 style={{ margin: 0 }}>Engineer</h1>
-                <h1 style={{ margin: 0, color: "#B517F5" }}>@ Oracle</h1>
+                <h1 className="title-experience" style={{ margin: 0 }}>Engineer</h1>
+                <h1 className="company-experience" style={{ margin: 0, color: "#B517F5" }}>@ Oracle</h1>
                 </div>
                 <p style={{ marginTop: 0, marginBottom: "12px" }}>
                 June 2024 - March 2025
                 </p>
                 <ul style={{ marginTop: 0,listStyleType: "disc"}}>
-                <li style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Developed an Oracle tools application, reducing search time by 80%. Previously, engineers navigated multiple pages,
+                <li className="bullet-experience" style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Developed an Oracle tools application, reducing search time by 80%. Previously, engineers navigated multiple pages,
                 taking 2-5 minutes on average.</li>
-                <li style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Implemented a global support contact page, reducing search time by 10–15 minutes and improving weekend availability.</li>
+                <li className="bullet-experience" style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Implemented a global support contact page, reducing search time by 10–15 minutes and improving weekend availability.</li>
                 </ul>
             </div>
         )}
@@ -99,15 +99,15 @@ function ExperienceTabs() {
         {activeTab === "third" && (
             <div>
                 <div style={{ display: "flex", gap: "8px", marginBottom: "4px" }}>
-                <h1 style={{ margin: 0 }}>Engineer</h1>
-                <h1 style={{ margin: 0, color: "#B517F5" }}>@ Grupo Luthe</h1>
+                <h1 className="title-experience" style={{ margin: 0 }}>Engineer</h1>
+                <h1 className="company-experience" style={{ margin: 0, color: "#B517F5" }}>@ Grupo Luthe</h1>
                 </div>
                 <p style={{ marginTop: 0, marginBottom: "12px" }}>
                 February 2023 - January 2024
                 </p>
                 <ul style={{ marginTop: 0,listStyleType: "disc"}}>
-                <li style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Developed administrative Oracle APEX pages for small businesses, streamlining daily operations and improving efficiency.</li>
-                <li style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Assisted in migrating applications to a new Oracle APEX version, ensuring smooth transition and minimal downtime.s</li>
+                <li className="bullet-experience" style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Developed administrative Oracle APEX pages for small businesses, streamlining daily operations and improving efficiency.</li>
+                <li className="bullet-experience" style={{ marginBottom: "10px", fontFamily: "'Poppins', sans-serif"}}>Assisted in migrating applications to a new Oracle APEX version, ensuring smooth transition and minimal downtime.s</li>
                 </ul>
             </div>
         )}
